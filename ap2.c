3660e0ec82a3480708f9a760481d607db0da92aa
lily77777 <55526154+lily77777@users.noreply.github.com>
2020-04-10 15:02:01 +0900

    자료구조과제

---
+++ ap2.c
@@ -0,0 +1,33 @@
+/*
+ * ap2.c
+ *
+ *  Created on: 2020. 4. 9.
+ *      Author: user
+ */
+
+/*#include<stdio.h>
+#include<stdlib.h>
+
+void main(){
+	int list[5];
+	int *plist[5];
+
+	list[0]=10;
+	list[1]=11;
+
+	plist[0]=(int*)malloc(sizeof(int));
+	printf("software sunyilin 2016039102\n");
+	printf("list[0]\t=%d\n",list[0]);
+	printf("address of list\t=%p\n",list);
+	printf("address of list[0]\t=%p\n",&list[0]);
+	printf("address of list +0 \t=%p\n",list+0);
+	printf("address of list +1\t=%p\n",list+1);
+	printf("address of list +2\t=%p\n",list+2);
+	printf("address of list +3\t=%p\n",list+3);
+	printf("address of list +4\t=%p\n",list+4);
+	printf("address of list[4]\t=%p\n",&list[4]);
+
+	free(plist[0]);
+
+}*/
+
---
+++ p22.c
@@ -0,0 +1,32 @@
+/*
+ * p22.c
+ *
+ *  Created on: 2020. 4. 10.
+ *      Author: user
+ */
+/*#include<stdio.h>
+void print1(int *ptr,int rows);
+
+int main()
+{
+	int one[]={0,1,2,3,4};
+
+	printf("software 2016039102 sunyilin\n");
+	printf("one           =%p\n",one);
+	printf("&one          =%p\n",&one);
+	printf("&one[0]       =%p\n",&one[0]);
+	printf("\n");
+
+	print1(&one[0],5);
+
+	return 0;
+}
+
+void print1(int *ptr,int rows){
+	int i;
+	printf("Address\t Contents\n");
+	for(i=0;i<rows;i++)
+		printf("%p\t  %5d\n",ptr +i,*(ptr+i));
+	printf("\n");
+}*/
+
